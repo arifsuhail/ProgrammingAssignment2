ec0ce05a1aeccdbd9439761627ca426cb0055e52
arifsuhail <arifsuhail@zhcet.ac.in>
2014-12-18 08:27:47 -0800

    my Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## use the following function to make a special matrix
+## for example you may write the following for 3x3 matrix as below
+##the numbers are shown just as an example
+## a<- makeCacheMatrix(matrix(c(1,5,8,3,8,9,2,4,7),3,3))
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x= matrix())
+{
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+##After, for example, a is made as above, you may use the following function
+##to get the inverse of the matrix by calling as
+## cacheSolve(a)
+##if you use again cacheSolve(a), you will get the cached value
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+  m <- x$getinverse()
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
